a6a3a7a249d3b11e8ced4bdf853141a791595030
alejandroava <alejandroalzate.ava@gmail.com>
2024-06-02 21:55:08 -0500

    add scene task , fecth task

--- app/scenes/task/task.scene.js
+++ app/scenes/task/task.scene.js
@@ -1,17 +1,60 @@
+import { fetchApi } from "../../helpers/fetch.api"
 export function TaskScene() {
     const pageContent = `
-        <div>
-        <hi>Vista de tareas</h1>
-        <ul>
-            <li>Tarea 1</li>
-            <li>Tarea 2</li>
-            <li>Tarea 3</li>
-        </ul>
+    <form>
+        <h1>Tareas</h1>
+        <input type="text" placeholder="Titulo de la tarea" name="title">
+        <input type="text" placeholder="Descripcion" name="description">
+        <select name='select'>
+            <option value="Alta">Alta</option>
+            <option value="Media">Media</option>
+            <option value="Baja">Baja</option>
+        </select>
+        <input type="date" name="date">
+        <button type="submit">Guardar tarea</button>
+    </form>
+    <div id='taskContainer'></div>
 
-    </div>
     `
-    const logic = () => {
-        console.log('hola desde task')
+    const logic = async () => {
+        const $myFormTask = document.getElementsByTagName('form')[0]
+        const allTask = document.getElementById('taskContainer')
+
+        const showTask = await fetchApi('http://localhost:3000/tasks') 
+        showTask.forEach(task => {
+            allTask.innerHTML += `
+            <p>${task.title}</p>
+            <button>Editar</button>
+            <button>Eliminar</button>
+            <button>Vista Previa</button>
+            `
+
+        });
+
+        $myFormTask.addEventListener('submit', async e => {
+            e.preventDefault()
+            const formData = new FormData(e.target)
+            const title = formData.get('title')
+            const description = formData.get('description')
+            const select = formData.get('select')
+            const date = formData.get('date')
+
+            
+
+
+            const taskCreated = await fetchApi('http://localhost:3000/tasks', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    title,
+                    description,
+                    select,
+                    date,
+                })
+            })
+       })
     }
     return {
         pageContent,
